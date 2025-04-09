b42d86d3cceb6517ef4f96887d12c377f3fc70a8
dmacisso <dmacisso@gmail.com>
2025-04-09 11:15:53 -0400

    Chapter 3.4 "Product Display" and Chapter 3.5 "Product Styles"

--- components/ImageBanner.jsx
+++ components/ImageBanner.jsx
@@ -1,3 +1,47 @@
+'use client';
+
+import { useState, useRef, useEffect } from 'react';
+
+//* use client is needed because this component is interactive (an image loading event)
 export default function ImageBanner() {
-  return <div className="banner-images">ImageBanner</div>;
+  const [isloaded, setIsloaded] = useState(false);
+  const imgRef = useRef();
+
+  useEffect(() => {
+    //* is the image completely loaded? Is the current status "complete"?
+    if (imgRef.current.complete) setIsloaded(true);
+  }, []);
+  return (
+    <div className="banner-images">
+      <img
+        className="low-res-img"
+        src="low_res/banner.jpeg"
+        alt="banner-low-resolution"
+      />
+      <img
+        ref={imgRef}
+        src="med_res/banner.png"
+        alt="banner-high-res"
+        className="high-res-img"
+        style={{ opacity: isloaded ? 1 : 0 }}
+        // 1 is visible
+        onLoad={() => {
+          //* this cb function is executed when the high res image is loaded and subsequently made visible
+          setIsloaded(true);
+        }}
+      />
+      <div className="cta-btns-container">
+        <div>
+          <div>
+            <h3 className="">Welcome To</h3>
+            <h1>The DVM Store</h1>
+          </div>
+          <div>
+            <button>Shop Stickers</button>
+            <button>Shop Planner</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/Products.jsx
+++ components/Products.jsx
@@ -1,3 +1,104 @@
+'use client';
+
 export default function Products() {
-  return <div>Products</div>;
+  const stickerDescriptions = {
+    CSS_HTML_Javascript: 'Core...',
+    Docker: 'Platform..',
+    Firebase: 'Cloud ...',
+    NextJS: 'React-based...',
+    NodeJS: 'JavaScript runtime',
+    PostgreSQL: 'Robust',
+    ReactJS: 'JavaScript library',
+  };
+
+  const stickers = Object.keys(stickerDescriptions);
+
+  return (
+    <>
+      <div className="section-container">
+        <div className="section-header">
+          <h2 className="">Shop Our Selection</h2>
+          <p>From organization to accessorization</p>
+        </div>
+        <div className="planner-container">
+          <div>
+            <button className="img-button">
+              <img src="/low_res/planner.jpeg" alt="high-res-planner" />
+            </button>
+          </div>
+          <div className="planner-info">
+            <p className="text-large planet-header">
+              Medieval Dragon Month Planner
+            </p>
+            <h3>
+              <span>$</span>14.99
+            </h3>
+            <p>
+              Step into a realm of fantasy and organization with our{' '}
+              <strong>Medieval Dragon Month Planner</strong>! This
+              high-resolution PNG asset combines the fierce elegance of dragons
+              with intricate medieval designs to create a planner that's not
+              only functional but also a work of art. Whether you&apos;re
+              jotting down quests, planning battles, or just scheduling your
+              weekly grocery run, this planner is your ultimate companion.
+            </p>
+            <ul>
+              <li>
+                <strong>Epic Dragon Artwork:</strong> Stunning hand-drawn dragon
+                motifs and medieval-inspired borders make every month feel
+                legendary.
+              </li>
+              <li>
+                <strong>Fully Printable:</strong> Designed at ultra-high
+                resolution, it&apos;s perfect for printing on any size paper,
+                from A4 to poster-sized displays.
+              </li>
+            </ul>
+            <div className="purchase-btns">
+              <button
+              // onClick={() => {
+              //   const plannerPriceId = planner.default_price;
+              //   handleIncrementProduct(plannerPriceId, 1, planner);
+              // }}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-container">
+        <div className="section-header">
+          <h2 className="">Or collect your Favorite Tech</h2>
+          <p>Choose from our custom design tech logos</p>
+        </div>
+        <div className="sticker-container">
+          {stickers.map((sticker, stickerIndex) => {
+            return (
+              <div className="sticker-card" key={stickerIndex}>
+                <button className="img-button">
+                  <img
+                    src={`low_res/${sticker}.jpeg`}
+                    alt={`${sticker}-low-res`}
+                  />
+                </button>
+                <div className="sticker-info">
+                  <p className="text-medium">
+                    {sticker.replaceAll('_', ' ')} Sticker.png
+                  </p>
+                  <p>{stickerDescriptions[sticker]}</p>
+                </div>
+                <h4>
+                  <span>$</span>5.99
+                </h4>
+
+                <button>Add to cart</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
