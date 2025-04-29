605aac5f0aa6609bfd4e0ced6dea7652d8977e34
dmacisso <dmacisso@gmail.com>
2025-04-29 11:25:05 -0400

    Chapter 3.11 - Create Stripe Checkout again

---
+++ app/success/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function SuccessPage() {
+  return (
+    <div className="page-container">
+      <h2 className="text-large">Thank you for your purchase 🙏</h2>
+      <Link href="/">
+        <button className="">Continue Shopping &rarr;  </button>
+      </Link>
+    </div>
+  );
+}
